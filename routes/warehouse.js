f8e439d5a569ea310c8ac9af0e0f233bcaf57136
Georgi Baldjiev <gbaldjiev2004@gmail.com>
2020-01-04 17:44:49 +0200

    added responses to operation events

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -7,16 +7,23 @@ function isEmpty(value) {
 }
 
 function isRequestValid(req, actionType) {
-    if (isEmpty(req.body)) return false;
-    switch (actionType) {
-        case 'import':
-            return !isEmpty(req.body.token) && !isEmpty(req.body.slot) && !isEmpty(req.body.item) && !isEmpty(req.body.item.name) && !isEmpty(req.body.item.description);
-        case 'export':
-            return !isEmpty(req.body.token) && !isEmpty(req.body.item_address) && !isEmpty(req.body.slot);
-        case 'list':
-            return !isEmpty(req.body.token);
-        default:
-            return false;
+    try{
+        if (isEmpty(req.body)) return false;
+        switch (actionType) {
+            case 'import':
+                return !isEmpty(req.body.token) && !isEmpty(req.body.slot) && !isEmpty(req.body.item) && !isEmpty(req.body.item.name) && !isEmpty(req.body.item.description);
+            case 'export':
+                return !isEmpty(req.body.token) && !isEmpty(req.body.item_address) && !isEmpty(req.body.slot);
+            case 'list':
+                return !isEmpty(req.body.token);
+            case 'operation':
+                return !isEmpty(req.body.operation.id) && !isEmpty(req.body.operation.status);
+            default:
+                return false;
+        }
+    }catch(error){
+        console.log(error);
+        return false;
     }
 }
 
@@ -65,6 +72,36 @@ router.post('/list', async function (req, res) {
         data: null
     };
     result.data = await database.manyOrNone("SELECT address, name, description, time_filled, status FROM storageUnits WHERE owner_id=(SELECT id FROM users WHERE token = $(token))", req.body);
+    res.json(result);
+})
+
+router.post('/operation_event', async function(req, res){
+    var result = {
+        error: [],
+        data: null
+    };
+    
+    if(!isRequestValid(req, 'operation')) result.error.push("invalidRequest");
+    if(result.error.length==0){
+        switch(req.body.operation.status){
+            case 'started':
+                await database.none("UPDATE operations SET status='processing' WHERE id=$(operation.id)", req.body);
+                break;
+            case 'error':
+                await database.none("UPDATE storageUnits SET owner_id=NULL, name=NULL, time_filled=NULL, description=NULL, status='vacant' WHERE address=(SELECT destination FROM operations WHERE id=$(operation.id))", req.body);
+                await database.none("DELETE FROM operations WHERE id=$(operation.id)", req.body);
+                //report error to client
+                break;
+            case 'complete':
+                await database.none("UPDATE storageUnits SET status='taken' WHERE address=(SELECT destination FROM operations WHERE id=$(operation.id))", req.body);
+                await database.none("DELETE FROM operations WHERE id=$(operation.id)", req.body);
+                break;
+            default:
+                result.error.push("invalidOperationStatus");
+                break;
+        }
+    }
+
     res.json(result);
 })
 
